abb057423b881c678e7f7c85e1405e0a1cb9e74b
tommykurniawans <tommy.kurniawan.s@gmail.com>
2020-09-19 19:02:08 +0700

    first commit

---
+++ src/test/java/com/fullstack/batch3/BaseAPITest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.fullstack.batch3;
+
+import com.fullstack.batch3.common.DataUtils;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+/**
+ * @author chandan
+ *
+ */
+public abstract class BaseAPITest {
+
+	protected RequestSpecification commonSpec = new RequestSpecBuilder()
+			.setBaseUri(DataUtils.getDataFromExcel("APIData", "ServerUrl")).setContentType("application/json").build()
+			.log().all();
+
+	protected <T> T getDataFromJsonPath(Response response, String jsonPath) {
+		JsonPath jPath = response.jsonPath();
+		
+		return jPath.get(jsonPath);
+	}
+}
